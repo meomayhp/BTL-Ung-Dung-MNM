bb5174d7843232c7826e9336bfabb9a71b49464c
meomayhp <dongxuanviethp@gmail.com>
2018-03-04 15:14:54 +0700

    BTL Phat trien ung dung ma nguon mo

---
+++ FlappyBird/desktop/src/com/vietcnt/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vietcnt.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vietcnt.game.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
