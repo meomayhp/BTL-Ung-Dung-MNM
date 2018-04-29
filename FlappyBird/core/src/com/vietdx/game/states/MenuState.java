8b0228195e645b00260d600970c4d112c143af57
meomayhp <dongxuanviethp@gmail.com>
2018-04-29 22:35:48 +0700

    Dong Xuan Viet - 56945 - Flappy Bird Game

---
+++ FlappyBird/core/src/com/vietdx/game/states/MenuState.java
@@ -0,0 +1,48 @@
+package com.vietdx.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.vietdx.game.FlappyBird;
+
+public class MenuState extends State{
+    private Texture background;
+    private Texture playButton;
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        cam.setToOrtho(false, FlappyBird.WIDTH/2, FlappyBird.HEIGHT /2);
+        background = new Texture("background.jpg");
+        playButton = new Texture("playbtn.png");
+    }
+
+    @Override
+    public void handleInput() {
+        if(Gdx.input.justTouched())
+        {
+            gsm.set(new PlayState(gsm));
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+        handleInput();
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+        sb.draw(playButton, cam.position.x - playButton.getWidth()/2, cam.position.y);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playButton.dispose();
+        System.out.println("Menu State Disposed");
+    }
+}
---
+++ FlappyBird/desktop/src/com/vietdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.vietdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vietdx.game.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = FlappyBird.WIDTH;
+		config.height = FlappyBird.HEIGHT;
+		config.title = FlappyBird.TITLE;
+		new LwjglApplication(new FlappyBird(), config);
+
+	}
+}
